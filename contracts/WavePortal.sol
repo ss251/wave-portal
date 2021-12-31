9d9922bd0b5e8e256dbcb438ff776d9325af750c
ss251 <ss251@uw.edu>
2021-12-31 16:18:23 -0600

    update contract to send eth to waver

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -18,7 +18,7 @@ contract WavePortal {
     /* hold all the waves sent */
     Wave[] waves;
 
-    constructor() {
+    constructor() payable {
         console.log("this is a smart contract");
     }
 
@@ -30,6 +30,14 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.00001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
